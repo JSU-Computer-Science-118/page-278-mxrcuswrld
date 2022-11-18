c50f864222c338d69fc2ea13558a6a39957c569b
mxrcuswrld <84354998+mxrcuswrld@users.noreply.github.com>
2022-11-17 23:47:27 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,10 +17,7 @@ int main()
 
   cout<<fixed<<showpoint<<setprecision(2);
 
-  cout<<"Line 14: Enter each volunteer's name and"
-      <<"the number of boxes"<< endl
-      <<"         sold by each volunteer, ending "
-      <<"with -1: " <<endl;
+  cout<<"Input:"<< endl;
 
   totalNumOfBoxesSold=0;
   numOfVolunteers=0;
@@ -37,24 +34,22 @@ int main()
       }
   cout<<endl;
 
-  cout<<"Line 26:The total number of boxes sold:"
+  cout<<"Output: Total number of boxes sold:"
       <<totalNumOfBoxesSold<<endl;
 
-  cout<<"Line 27: Enter the cost of one box:";
+  
   cin>>costOfOneBox;
   cout<<endl;
 
-  cout<<"Line 30: The tital money made by selling"
-      <<"cookies: $"
+  cout<<"Total money made:$"
       <<totalNumOfBoxesSold*costOfOneBox<<endl;
 
   if (numOfVolunteers !=0)
-    cout<<"Line 32: The average number of"
-        <<"boxes sold by each volunteer:"
+    cout<<"Average number of boxes sold by each person:"
         <<totalNumOfBoxesSold/numOfVolunteers 
         <<endl;
   else
-    cout<<"Line 34: No input."<<endl;
+    cout<<"No input."<<endl;
   return 0;
     
       
